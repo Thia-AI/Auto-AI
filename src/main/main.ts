fb6cab370ee02a409ff2358fb480422184c71c32
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2021-04-26 17:22:59 -0400

    Testing MR

--- src/main/main.ts
+++ src/main/main.ts
@@ -96,7 +96,7 @@ function initRendererDev(isDev: boolean): void {
 function launchEngine(): void {
 	/* eslint-disable  @typescript-eslint/no-unused-vars */
 	if (isDev) {
-		engineShell = EngineHandler.getInstance().createDevEngine(mainWindow);
+		engineShell = EngineHandler.getInstance().createDevEngine(mainWindow, true);
 	} else {
 		engineShell = EngineHandler.getInstance().createProdEngine(mainWindow);
 	}
