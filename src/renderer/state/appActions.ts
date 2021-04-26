a44eb1a2a078803426e11ffacbeeb6985ff535f4
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2021-04-26 17:36:43 -0400

    Testing bot

--- src/renderer/state/appActions.ts
+++ src/renderer/state/appActions.ts
@@ -2,6 +2,7 @@ import { ChooseModelActions } from './choose-model/model/actionTypes';
 import { EngineStatusActionTypes } from './engine-status/model/actionTypes';
 import { HeaderActionTypes } from './header/model/actionTypes';
 
+// Type union representing all action types from all features
 type AppActions = EngineStatusActionTypes | HeaderActionTypes | ChooseModelActions;
 
 export default AppActions;
