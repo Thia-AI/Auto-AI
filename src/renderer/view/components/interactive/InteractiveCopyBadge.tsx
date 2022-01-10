5d1047abbd2cf19783451da5319f97d87c07c6de
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2022-01-10 04:13:37 -0500

    ENG-120: Removed tooltip from interactive badge (much more performance if HTML title attribute is used instead)

--- src/renderer/view/components/interactive/InteractiveCopyBadge.tsx
+++ src/renderer/view/components/interactive/InteractiveCopyBadge.tsx
@@ -1,14 +1,13 @@
 import React, { useRef } from 'react';
 
-import { Badge, Tooltip, Text, Placement } from '@chakra-ui/react';
+import { Badge, Text } from '@chakra-ui/react';
 
 import './InteractiveCopyBadge.css';
 
 interface Props {
 	badgeID: string | undefined;
 	lengthToClip?: number;
-	tooltipLabel?: string;
-	tooltipPlacement?: Placement;
+	hoverLabel?: string;
 }
 /**
  * A badge that allows for copying on click.
@@ -26,37 +25,32 @@ export const InteractiveCopyBadge = React.memo((props: Props) => {
 		}
 	};
 	return (
-		<Tooltip
-			label={props.tooltipLabel || 'Copy ID'}
-			aria-label='A tooltip'
-			fontSize='xs'
-			placement={props.tooltipPlacement || 'left'}>
-			<Badge
-				maxW='full'
-				cursor='pointer'
-				ref={badgeIDRef}
-				my='2'
-				px='1'
-				opacity='0.6'
-				bg='gray.900'
-				onClick={(e) => {
-					// Prevent onclick from propogating up
-					e.stopPropagation();
-					// Notify via tooltip of copy
-					// Add bouncing animation and copy model ID to clipboard
-					badgeIDRef.current?.classList.add('copy-bounce');
-					navigator.clipboard.writeText(props.badgeID ? props.badgeID : '');
-					// remove bouncing animation after it's finished
-					setTimeout(() => {
-						if (badgeIDRef.current) {
-							badgeIDRef.current.classList.remove('copy-bounce');
-						}
-					}, 150);
-				}}>
-				<Text w='full' h='full' color='gray.600' isTruncated>
-					{renderBadgeID()}
-				</Text>
-			</Badge>
-		</Tooltip>
+		<Badge
+			maxW='full'
+			cursor='pointer'
+			ref={badgeIDRef}
+			my='2'
+			title={props.hoverLabel ?? 'Copy ID'}
+			px='1'
+			opacity='0.6'
+			bg='gray.900'
+			onClick={(e) => {
+				// Prevent onclick from propogating up
+				e.stopPropagation();
+				// Notify via tooltip of copy
+				// Add bouncing animation and copy model ID to clipboard
+				badgeIDRef.current?.classList.add('copy-bounce');
+				navigator.clipboard.writeText(props.badgeID ? props.badgeID : '');
+				// remove bouncing animation after it's finished
+				setTimeout(() => {
+					if (badgeIDRef.current) {
+						badgeIDRef.current.classList.remove('copy-bounce');
+					}
+				}, 150);
+			}}>
+			<Text w='full' h='full' color='gray.600' isTruncated>
+				{renderBadgeID()}
+			</Text>
+		</Badge>
 	);
 });
