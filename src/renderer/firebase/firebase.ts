ec78c94acefa5c7044e607e2e2e729d4a0e64859
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2022-04-11 19:06:48 -0400

    Added firebase config

---
+++ src/renderer/firebase/firebase.ts
@@ -0,0 +1,22 @@
+import { FirebaseOptions } from 'firebase/app';
+
+/**
+ * Firebase Config.
+ */
+export const firebaseConfig: FirebaseOptions = {
+	apiKey: 'AIzaSyDJOaqbWCPHazZsu4uyXCcYl69bSzr-hdc',
+	authDomain: 'thia-1458b.firebaseapp.com',
+	projectId: 'thia-1458b',
+	storageBucket: 'thia-1458b.appspot.com',
+	messagingSenderId: '564483189530',
+	appId: '1:564483189530:web:62e754e4173dfb80fd1aa3',
+	measurementId: 'G-PFW73XVXV6',
+};
+
+/**
+ * Firebase Admin Config.
+ */
+export const firebaseAdminConfig = {
+	serviceAccountId: 'firebase-adminsdk-e60qi@thia-1458b.iam.gserviceaccount.com',
+	projectId: 'thia-1458b',
+};
