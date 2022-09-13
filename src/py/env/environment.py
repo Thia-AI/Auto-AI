66b408209de09535545cc44bdb9ec2fad5e93006
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2022-09-13 16:56:58 -0400

    ENG-223: Fixed bug where Engine didn't set CUDA environ in sub processes

--- src/py/env/environment.py
+++ src/py/env/environment.py
@@ -11,6 +11,49 @@ from job.job import JobManager
 from log.logger import log
 
 
+def init_common_env(environment) -> None:
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        # Production
+        config.bundle_dir = Path(sys._MEIPASS)  # pylint: disable=no-member,protected-access
+        if environment == 'simulated':
+            """When Engine is ran in simulated mode during App development"""
+            # CUDA
+            config.CUDA_PATH = config.current_dir / 'extraResources' / 'engine' / 'CUDA'
+            # External dependencies
+            config.EXTERNAL_DEPENDENCIES = config.current_dir / 'extraResources' / 'engine' / config.EXTERNAL_DEPENDENCIES_DIR_NAME
+        else:
+            """When Engine is ran in actual production environment"""
+            # CUDA
+            config.CUDA_PATH = config.current_dir / 'resources' / 'extraResources' / 'engine' / 'CUDA'
+            # External dependencies
+            config.EXTERNAL_DEPENDENCIES = config.current_dir / 'resources' / 'extraResources' / 'engine' / config.EXTERNAL_DEPENDENCIES_DIR_NAME
+    else:
+        # Development
+        config.bundle_dir = Path(__file__).parent
+        if environment == 'pycharm':
+            """When Engine is ran in development mode via PyCharm"""
+            # CUDA
+            config.CUDA_PATH = config.current_dir.absolute().parent.parent / 'CUDA'
+            # External dependencies
+            config.EXTERNAL_DEPENDENCIES = config.current_dir.absolute().parent.parent / config.EXTERNAL_DEPENDENCIES_DIR_NAME
+
+        else:
+            """When Engine is ran in development mode via App"""
+            # CUDA
+            config.CUDA_PATH = config.current_dir.parent.parent.parent / 'CUDA'
+            # External dependencies
+            config.EXTERNAL_DEPENDENCIES = config.current_dir.parent.parent.parent / config.EXTERNAL_DEPENDENCIES_DIR_NAME
+
+    # Set environment variables
+    # CUDA
+    add_path_to_path_env(config.CUDA_PATH)
+    # Set CUDA_DEVICE_ORDER so the IDs assigned by CUDA match those from nvidia-smi
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    # External Dependencies
+    config.ED_VIPS = config.EXTERNAL_DEPENDENCIES / config.ED_VIPS_DIR_NAME
+    add_path_to_path_env(config.ED_VIPS)
+
+
 def init_environment_pre_gpu(args) -> None:
     config.USER_UID = args.user
     user_data = Path(args.user_data)
@@ -28,10 +71,6 @@ def init_environment_pre_gpu(args) -> None:
             config.DATASET_DIR = user_data / config.USERS_DIR_NAME / args.user / config.DATASET_DIR_NAME
             # Database
             config.DATABASE_LOCATION = user_data / config.USERS_DIR_NAME / args.user / config.DATABASE_DIR_NAME / config.DATABASE_FILE_NAME
-            # CUDA
-            config.CUDA_PATH = config.current_dir / 'extraResources' / 'engine' / 'CUDA'
-            # External dependencies
-            config.EXTERNAL_DEPENDENCIES = config.current_dir / 'extraResources' / 'engine' / config.EXTERNAL_DEPENDENCIES_DIR_NAME
         else:
             """When Engine is ran in actual production environment"""
             # Model
@@ -41,10 +80,6 @@ def init_environment_pre_gpu(args) -> None:
             config.DATASET_DIR = user_data / config.USERS_DIR_NAME / args.user / config.DATASET_DIR_NAME
             # Database
             config.DATABASE_LOCATION = user_data / config.USERS_DIR_NAME / args.user / config.DATABASE_DIR_NAME / config.DATABASE_FILE_NAME
-            # CUDA
-            config.CUDA_PATH = config.current_dir / 'resources' / 'extraResources' / 'engine' / 'CUDA'
-            # External dependencies
-            config.EXTERNAL_DEPENDENCIES = config.current_dir / 'resources' / 'extraResources' / 'engine' / config.EXTERNAL_DEPENDENCIES_DIR_NAME
     else:
         # Development
         config.bundle_dir = Path(__file__).parent
@@ -59,10 +94,6 @@ def init_environment_pre_gpu(args) -> None:
             config.DATASET_DIR = user_data / config.USERS_DIR_NAME / args.user / config.DATASET_DIR_NAME
             # Database
             config.DATABASE_LOCATION = user_data / config.USERS_DIR_NAME / args.user / config.DATABASE_DIR_NAME / config.DATABASE_FILE_NAME
-            # CUDA
-            config.CUDA_PATH = config.current_dir.absolute().parent.parent / 'CUDA'
-            # External dependencies
-            config.EXTERNAL_DEPENDENCIES = config.current_dir.absolute().parent.parent / config.EXTERNAL_DEPENDENCIES_DIR_NAME
 
         else:
             """When Engine is ran in development mode via App"""
@@ -73,19 +104,6 @@ def init_environment_pre_gpu(args) -> None:
             config.DATASET_DIR = user_data / config.USERS_DIR_NAME / args.user / config.DATASET_DIR_NAME
             # Database
             config.DATABASE_LOCATION = user_data / config.USERS_DIR_NAME / args.user / config.DATABASE_DIR_NAME / config.DATABASE_FILE_NAME
-            # CUDA
-            config.CUDA_PATH = config.current_dir.parent.parent.parent / 'CUDA'
-            # External dependencies
-            config.EXTERNAL_DEPENDENCIES = config.current_dir.parent.parent.parent / config.EXTERNAL_DEPENDENCIES_DIR_NAME
-
-    # Set environment variables
-    # CUDA
-    add_path_to_path_env(config.CUDA_PATH)
-    # Set CUDA_DEVICE_ORDER so the IDs assigned by CUDA match those from nvidia-smi
-    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    # External Dependencies
-    config.ED_VIPS = config.EXTERNAL_DEPENDENCIES / config.ED_VIPS_DIR_NAME
-    add_path_to_path_env(config.ED_VIPS)
 
     config.current_dir = Path.cwd() / config.bundle_dir
     msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
