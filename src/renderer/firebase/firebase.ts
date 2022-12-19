58ab0ee4fabc12c15daf648af70e34a5da749ab8
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2022-12-19 00:07:02 -0500

    Made firebase key private

--- src/renderer/firebase/firebase.ts
+++ src/renderer/firebase/firebase.ts
@@ -1,23 +1,6 @@
 import { FirebaseOptions } from 'firebase/app';
-
-const firebaseProdConfig: FirebaseOptions = {
-	apiKey: 'AIzaSyDJOaqbWCPHazZsu4uyXCcYl69bSzr-hdc',
-	authDomain: 'thia-1458b.firebaseapp.com',
-	projectId: 'thia-1458b',
-	storageBucket: 'thia-1458b.appspot.com',
-	messagingSenderId: '564483189530',
-	appId: '1:564483189530:web:62e754e4173dfb80fd1aa3',
-	measurementId: 'G-PFW73XVXV6',
-};
-
-const firebaseDevConfig: FirebaseOptions = {
-	apiKey: 'AIzaSyAoiWNvP_NuTIoXQ-oghg3Ix9jWLblQXcU',
-	authDomain: 'thia-dev.firebaseapp.com',
-	projectId: 'thia-dev',
-	storageBucket: 'thia-dev.appspot.com',
-	messagingSenderId: '554589208456',
-	appId: '1:554589208456:web:33c36d197c8467b1f6443d',
-};
+// Create config file with firebase config objects if doesn't exist already
+import { firebaseDevConfig, firebaseProdConfig } from './_config';
 
 /**
  * Gets Firebase config depending on if **App** is running in production or development.
